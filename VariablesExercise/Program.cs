fdbe9e29e3c274a17012f692416b55f6119951ce
David Armstrong <wafflesultimate1@gmail.com>
2023-09-28 14:13:49 -0700

    commit all changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Hans";
+
+            int number = 7;
+
+            char letter = 'a';
+
+            bool valuable = true;
+
+            double jadePrice = 5.6;
+
+            decimal rubyPrice = 4.3m;
+
+            Console.WriteLine($"{name} had {number} valuable stones. The exact price of the jade he had was {jadePrice} million.");
+
         }
     }
 }
